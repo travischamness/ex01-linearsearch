e968d32b4a4debd964b9562bf4109f460fc3c4f2
VC Science Labs <vscilab@vcccd.edu>
2018-09-10 14:43:37 -0700

    used csv15::find

---
+++ src/main.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "linearSearch.h"
+using edu::vcccd::vc::csv15::find;
+
+int main(int argc, char *argv[]) {
+	int array[] = {3, 1, 2};
+	find(1, array, 3) {
+		return 1;
+	}
+}
