d780255ac6c6d809cab0bd990a31129cd9b58d29
VC Science Labs <vscilab@vcccd.edu>
2018-09-17 13:12:30 -0700

    turing in again

---
+++ include/linearSearch.h
@@ -0,0 +1,20 @@
+#include <cstdint>
+
+namespace edu {
+	namespace vcccd {
+		namespace vc {
+			namespace csv15 {
+
+				template <class T>
+				int64_t find(const T& value, T array[], size_t size) {
+					if (array == NULL) return -1;
+					for (int64_t i = 0; i < size; i++) {
+						if (value == array[i]) return i;
+						//The return is only here because we are making a generic code.
+					} return -1;
+				}	
+
+			}
+		}
+	}
+}
