2f1f3edd93e954633d0c7689dc628959b1c2da1d
VC Science Labs <travis_chamness1@my.vcccd.edu>
2018-09-17 13:48:22 -0700

    Turning in

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -4,7 +4,6 @@ namespace edu {
 	namespace vcccd {
 		namespace vc {
 			namespace csv15 {
-
 				template <class T>
 				int64_t find(const T& value, T array[], size_t size) {
 					if (array == NULL) return -1;
